f4054dc0365043f3accaf07413240b8fa46c98d9
cabmak <morin700@live.ca>
2021-02-11 14:26:03 -0500

    11/02

--- config/config.exs
+++ config/config.exs
@@ -26,6 +26,15 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+config :ueberauth, Ueberauth,
+  providers: [
+    github: {Ueberauth.Strategy.Github, [allow_private_emails: true]}
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "e4bd8f76cb3c417b13e7",
+  client_secret: "8d7fdaa9490cda4760e348a1d500fc6e3af4f189"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
---
+++ lib/discuss_web/controllers/plugs/set_user.ex
@@ -0,0 +1,15 @@
+defmodule DiscussWeb.Plugs.SetUser do
+  import Plug.Conn
+  import Phoenix.Controller
+
+  alias DiscussWeb.Repo
+  alias DiscussWeb.User
+  alias DiscussWeb.Router.Helpers
+
+  def init(_params) do
+  end
+
+  def call(conn, _params) do
+    
+  end
+end
